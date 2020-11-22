f0ec208fd3a68d411b0ef0d041f8dfc3662ae1dc
paruma <48xi3dparu@i.softbank.jp>
2020-11-23 07:57:44 +0900

    oembedを試した

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,9 @@
+require "oembed"
+
 class PostsController < ApplicationController
   def index
     @posts = Post.all
+    OEmbed::Providers.register_all
+    @twitter_html = OEmbed::Providers.get('http://www.youtube.com/watch?v=2BYXBC8WQ5k').html.html_safe
   end
 end
